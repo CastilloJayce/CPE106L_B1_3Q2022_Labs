753f5ef2d99f46f7ee43a0bc290a6bc6c5df524c
Miguel Rioveros <86728324+DonMig@users.noreply.github.com>
2022-05-03 23:42:30 +0800

    Update LR1 PostLab 1.py

--- LR1 PostLab 1.py
+++ LR1 PostLab 1.py
@@ -1,20 +1,17 @@
-#function mode to find mode
+
 def mode(lyst):
     lyst.sort() #sort the list
     list_elem = dict()
-    for i in lyst:#using for loop
-        #creating dictionary in python with key and count of each element
+    for i in lyst:
         list_elem[i] = list_elem.get(i, 0) + 1
-    #return maximun key from the list_elem
     return max(list_elem, key=list_elem.get)
 #function meadian to find median value
 def median(lyst):
-    lyst.sort() #sort the list
+    lyst.sort() 
     return lyst[int(len(lyst)/2)]
 #function mean to find mean of the list
 def mean(lyst):
-    sum=0 #declaring variable to store sum
-    #using for loop
+    sum=0 
     for i in range(0,len(lyst)):
         sum=sum+lyst[i] #add each value from lyst into variable sum
     #find and return mean
